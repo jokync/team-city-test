73685d830f1e6d2d68cc4ec55dbf8564e3a85874
Joel Colmenares <Joel.Khin.X.Colmenares.Barradas.-ND@disney.com>
2021-03-17 08:04:13 -0500

    adding a new feat for testing porpuses

--- app/src/main/java/com/example/teamcityintegration/MainActivity.kt
+++ app/src/main/java/com/example/teamcityintegration/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.teamcityintegration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+  override fun onClick(v: View?) {
+    TODO("Not yet implemented")
+  }
+
 }
